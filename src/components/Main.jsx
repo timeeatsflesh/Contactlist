b03849dfde30be37dd1d036a71d4b73c8d51c7d3
timeeatsflesh <Robert@Roberts-MacBook.local>
2023-02-15 17:01:43 -0700

    added api awate

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,21 @@ import dummyContacts from "../dummyData";
 import {ContactList} from "./"
 
 const Main = () => {
-  const [contacts, setContacts] = useState(dummyContacts);
+  const [contacts, setContacts] = useState([]);
+ 
+  const getContacts = async () => {
+    try {
+      const response = await fetch('http://jsonplace-univclone.herokuapp.com/users')
+      const contacts = await response.json()
+      setContacts(contacts)
+      console.log('got data')      
+    } catch (error) {
+      console.log('there is no data')
+    }
+
+  }
+
+
 
   return (
     <div id="main">
